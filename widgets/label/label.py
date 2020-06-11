ae9e407e90a6fe88ccaeb9e6d4551e432a4bdd23
Tresdon Jones <tresdonj@gmail.com>
2020-06-11 15:07:07 -0400

    Add content

--- widgets/label/label.py
+++ widgets/label/label.py
@@ -1,11 +1,38 @@
+# Static Text Label
 import tkinter
 
 root = tkinter.Tk()
+root.title("tkinterexamples.com")
 
-tkinter.Label(root, text="Hello, World!").pack()
+tkinter.Label(root, text="Hello, world!").pack()
+
+root.mainloop()
+
+
+# Dynamic Text Label
+import tkinter
+
+root = tkinter.Tk()
+root.title("tkinterexamples.com")
 
 text_var = tkinter.StringVar()
 text_var.set("Hello from a variable!")
 tkinter.Label(root, textvariable=text_var).pack()
+
+root.mainloop()
+
+
+# Image Label
+import tkinter
+from PIL import Image, ImageTk
+
+root = tkinter.Tk()
+root.title("tkinterexamples.com")
+
+pil_image = Image.open("/Users/tresdon/images_synced/photographs/pentax/california/split_tree.jpg")
+pil_image_resized = pil_image.resize((300, 200))
+tkinter_image = ImageTk.PhotoImage(pil_image_resized)
+
+tkinter.Label(root, image=tkinter_image).pack()
 
 root.mainloop()
