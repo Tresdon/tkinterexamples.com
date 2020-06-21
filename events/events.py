dbdeea2edcec5046216358f9e7393024d115d1ab
Tresdon Jones <tresdonj@gmail.com>
2020-06-20 23:02:54 -0400

    Remove false info

--- events/events.py
+++ events/events.py
@@ -1,3 +1,18 @@
+# Implicit
+import tkinter
+
+root = tkinter.Tk()
+
+
+tkinter.Checkbutton(root, command=lambda: print("x")).pack()
+tkinter.Radiobutton(root, command=lambda: print("x")).pack()
+tkinter.Scale(root, command=lambda x: print("x")).pack()
+tkinter.Scrollbar(root, command=lambda x: print("x")).pack()
+tkinter.Spinbox(root, command=lambda: print("x")).pack()
+
+root.mainloop()
+
+
 # Protocol
 import tkinter
 
